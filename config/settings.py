b17272bfe2c01687a9c192f778677f95e549b42e
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-05-30 19:26:11 +0300

    add: added sites to admin panel

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,7 @@ DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -29,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # my apps
     "core",
     "posts",
