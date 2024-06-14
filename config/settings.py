d12c7086d0f5c93fceac7b6bc54daae1b8922c3f
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-14 03:03:41 +0300

    add: added a logic to create a superuser since render is not offering a shell

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,18 @@
-import os
+from django.contrib.auth.models import User
 import dj_database_url
-from pathlib import Path
 from dotenv import load_dotenv
+import os
+from pathlib import Path
 
 load_dotenv()
+
+
+# Superuser creation logic
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'email@email.com', os.getenv('ADMIN_PASSWORD'))
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
