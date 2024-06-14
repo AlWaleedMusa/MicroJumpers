89e14b5eadd57c4b4b966decff31aaf7bd91a03a
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-06-14 03:29:20 +0300

    fix: fixed a mislogic regarding media files

--- config/settings.py
+++ config/settings.py
@@ -153,12 +153,12 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 if DEBUG:
     MEDIA_URL = "media/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 else:
-    MEDIA_URL = "https://www.dropbox.com/home/Apps/microjumpers/"
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+    MEDIA_URL = "https://www.dropbox.com/home/Apps/microjumpers/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
